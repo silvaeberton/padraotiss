9e3f6fa81dc3af5ca94f95661f3b8bd4efbbadfc
silvaeberton <44627034+silvaeberton@users.noreply.github.com>
2020-07-29 12:28:06 -0300

    Upload inicial
    Download funcionando corretamente

---
+++ PadraoTISS/src/Main.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {		
+		
+		URL url = new URL("http://www.ans.gov.br/images/stories/Plano_de_saude_e_Operadoras/tiss/Padrao_tiss/tiss3/Padrao_TISS_Componente_Organizacional_202006.pdf");
+		
+		File file = new File("Componente-organizacional.pdf");		
+		
+		InputStream is = url.openStream();
+		FileOutputStream fos = new FileOutputStream(file);
+
+        int bytes = 0;
+
+        while ((bytes = is.read()) != -1) {
+            fos.write(bytes);        
+        }       
+        
+        is.close();
+
+        fos.close();
+	}
+}
